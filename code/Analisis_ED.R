84a3ee7bf70c5fd2f64a2b363098df6d727c1cfa
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2023-02-21 15:30:47 -0600

    Se aplico factor a ambos atributos de interes

--- code/Analisis_ED.R
+++ code/Analisis_ED.R
@@ -4,3 +4,12 @@ library("limma")
 library("pheatmap")
 
 rse_gene_ERP110066 <- expand_sra_attributes(rse_gene_ERP110066)
+
+colData(rse_gene_ERP110066)[
+  ,
+  grepl("^sra_attribute", colnames(colData(rse_gene_ERP110066)))
+]
+
+## Se revisa que los atributos de interes tengan el formato adecuado
+rse_gene_ERP110066$sra_attribute.stimulus<-factor(tolower((rse_gene_ERP110066$sra_attribute.stimulus)))
+rse_gene_ERP110066$sra_attribute.growth_condition<-factor(tolower((rse_gene_ERP110066$sra_attribute.growth_condition)))
