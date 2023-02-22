f4152a1d2eb3a2219fc05e2e2f1f0cb0aba52c82
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2023-02-21 23:59:38 -0600

    Se genera la columna de proporcion de genes y se revisa el histograma

--- code/Analisis_ED.R
+++ code/Analisis_ED.R
@@ -13,3 +13,19 @@ colData(rse_gene_ERP110066)[
 ## Se revisa que los atributos de interes tengan el formato adecuado
 rse_gene_ERP110066$sra_attribute.stimulus<-factor(tolower((rse_gene_ERP110066$sra_attribute.stimulus)))
 rse_gene_ERP110066$sra_attribute.growth_condition<-factor(tolower((rse_gene_ERP110066$sra_attribute.growth_condition)))
+
+hist(rse_gene_ERP110066$assigned_gene_prop, xlab = "Assigned gene proportion", main = 'Assigned gene proportion')
+
+
+summary(as.data.frame(colData(rse_gene_ERP110066)[
+  ,
+  grepl("^sra_attribute.[growt_condition|stimulus]", colnames(colData(rse_gene_ERP110066)))
+]))
+
+
+rse_gene_ERP110066$assigned_gene_prop <- rse_gene_ERP110066$recount_qc.gene_fc_count_all.assigned/rse_gene_ERP110066$recount_qc.gene_fc_count_all.total
+
+summary(rse_gene_ERP110066$assigned_gene_prop)
+
+promedios_genes <- rowMeans(assay(rse_gene_ERP110066, "counts"))
+summary(promedios_genes)
