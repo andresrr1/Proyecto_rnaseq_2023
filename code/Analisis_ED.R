c0ef3f3b63e1f6140e510a82e96aadbe7a2471c0
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2023-02-25 00:02:45 -0600

    Se agrega el modelo estadistico, la funcion grafica voom, el volcano plot y el pheat map

--- code/Analisis_ED.R
+++ code/Analisis_ED.R
@@ -49,3 +49,58 @@ ggplot(as.data.frame(colData(rse_gene_ERP110066)), aes(y = assigned_gene_prop, x
   theme_bw(base_size = 20) +
   ylab("Assigned Gene Prop") +
   xlab("Growth condition")
+
+mod <- model.matrix(~sra_attribute.growth_condition + assigned_gene_prop,
+                    data = colData(rse_gene_ERP110066))
+
+colnames(mod)
+
+
+mod
+
+vGenes <- voom(DGE, mod, plot = T)
+
+eb_results <- eBayes(lmFit(vGenes))
+
+de_results <- topTable(
+  eb_results,
+  coef = 2,
+  number = nrow(rse_gene_ERP110066),
+  sort.by = "p"
+)
+
+
+head(de_results)
+dim(de_results)
+
+table(de_results$adj.P.Val<0.05)
+
+
+plotMA(eb_results, coef = 2)
+volcanoplot(eb_results, coef = 2, highlight = 3, names = de_results$gene_name)
+
+
+exprs_heatmap <- vGenes$E[rank(de_results$adj.P.Val) <= 50,]
+
+rownames(exprs_heatmap) <- de_results[row.names(exprs_heatmap),"gene_name"]
+
+
+df <- as.data.frame(colData(rse_gene_ERP110066)[, c("sra_attribute.growth_condition", "sra_attribute.stimulus")])
+colData(rse_gene_ERP110066)[, "sra_attribute.growth_condition","sra_attribute.stimulus" ]
+
+
+colnames(df) <- c("growth_condition", "stimulus")
+
+View(df)
+
+pheatmap(
+  exprs_heatmap,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  show_rownames = FALSE,
+  show_colnames = FALSE,
+  annotation_col = df
+)
+
+plotMDS(vGenes$E, labels = df$growth_condition, col = c("blue","red"))
+
